b809c0c22e5f3699c3edac2b3ee3b51f90890324
OreoDog1 <zubintalwar@gmail.com>
2022-03-05 00:42:49 +0000

    Made finding reviewers a function

--- reviews.py
+++ reviews.py
@@ -1,90 +1,92 @@
 import csv
 import random
 
-teams = {}
+def reviews(reviewers_csv, reviewees_csv):
+    teams = {}
 
-# Compile the Reviewers data
-experienced = {}
-reviewers = []
-with open("Reviews - Reviewers.csv", "r") as file:
-    reader = csv.DictReader(file)
-    for row in reader:
-        name = row["Reviewer"]
-        teams[name] = row["Team"]
-        experienced[name] = row["Experienced"]
-        reviewers.append(name)
+    # Compile the Reviewers data
+    experienced = {}
+    reviewers = []
+    with open(reviewers_csv, "r") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            name = row["Reviewer"]
+            teams[name] = row["Team"]
+            experienced[name] = row["Experienced"]
+            reviewers.append(name)
 
-# Compile the Reviewees data
-reviewees = []
-with open("Reviews - Reviewees.csv", "r") as file:
-    reader = csv.DictReader(file)
-    for row in reader:
-        teams[row["Reviewee"]] = row["Team"]
-        reviewees.append(row["Reviewee"])
+    # Compile the Reviewees data
+    reviewees = []
+    with open(reviewees_csv, "r") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            teams[row["Reviewee"]] = row["Team"]
+            reviewees.append(row["Reviewee"])
 
-# Create dictionary containing reviewer-to-reviewee matchings
-reviewed = {}
-for reviewee in reviewees:
-    reviewed[reviewee] = []
+    # Create dictionary containing reviewer-to-reviewee matchings
+    reviewed = {}
+    for reviewee in reviewees:
+        reviewed[reviewee] = []
 
-# Repeat until all are reviewed
-while True:
-    for reviewer in reviewers:
-        # Iterate through the reviewees in a random order
-        random.shuffle(reviewees)
-        for reviewee in reviewees:
-            # Find the current reviewers for the reviewee
-            chosen = reviewed[reviewee]
+    # Repeat until all are reviewed
+    while True:
+        for reviewer in reviewers:
+            # Iterate through the reviewees in a random order
+            random.shuffle(reviewees)
+            for reviewee in reviewees:
+                # Find the current reviewers for the reviewee
+                chosen = reviewed[reviewee]
 
-            # Check if the reviewee is valid
-            if teams[reviewee] != teams[reviewer] and len(chosen) < 2 and reviewer not in chosen:
-                # Make sure nobody gets two inexperienced reviewers
-                if len(chosen) == 1 and experienced[reviewer] == "n" and experienced[chosen[0]] == "n":
-                    continue
-                reviewed[reviewee].append(reviewer)
+                # Check if the reviewee is valid
+                if teams[reviewee] != teams[reviewer] and len(chosen) < 2 and reviewer not in chosen:
+                    # Make sure nobody gets two inexperienced reviewers
+                    if len(chosen) == 1 and experienced[reviewer] == "n" and experienced[chosen[0]] == "n":
+                        continue
+                    reviewed[reviewee].append(reviewer)
+                    break
+
+        # Check if everyone has been reviewed twice
+        all_reviewed = True
+        for assigned in reviewed.values():
+            if len(assigned) < 2:
+                all_reviewed = False
                 break
 
-    # Check if everyone has been reviewed twice
-    all_reviewed = True
-    for assigned in reviewed.values():
-        if len(assigned) < 2:
-            all_reviewed = False
+        if all_reviewed:
             break
 
-    if all_reviewed:
-        break
+    return (reviewed, reviewers, reviewees, experienced, teams)
 
-with open("output.csv", "w") as file:
-    writer = csv.writer(file)
-    writer.writerow(["Reviewee", "Team", "Reviewer1", "Reviewer2"])
+def test():
+    (reviewed, reviewers, reviewees, experienced, teams) = reviews("Reviews - Reviewers.csv", "Reviews - Reviewees.csv")
+
+    two_reviewers = True
+    one_experienced = True
+    different_team = True
+
     for reviewee in reviewees:
-        writer.writerow([reviewee, teams[reviewee], reviewed[reviewee][0], reviewed[reviewee][1]])
+        assigned = reviewed[reviewee]
+        if len(assigned) != 2:
+            two_reviewers = False
+            break
 
-# two_reviewers = True
-# one_experienced = True
-# different_team = True
+        if experienced[assigned[0]] == "n" and experienced[assigned[1]] == "n":
+            one_experienced = False
 
-# for reviewee in reviewees:
-#     assigned = reviewed[reviewee]
-#     if len(assigned) != 2:
-#         two_reviewers = False
-#         break
+        if teams[reviewee] == teams[assigned[0]] or teams[reviewee] == teams[assigned[1]]:
+            different_team = False
 
-#     if experienced[assigned[0]] == "n" and experienced[assigned[1]] == "n":
-#         one_experienced = False
+    num_reviews = {}
+    for reviewer in reviewers:
+        count = 0
+        for assigned in reviewed.values():
+            if reviewer in assigned:
+                count += 1
+        num_reviews[reviewer] = count
 
-#     if teams[reviewee] == teams[assigned[0]] or teams[reviewee] == teams[assigned[1]]:
-#         different_team = False
+    print(two_reviewers, "two reviewers")
+    print(one_experienced, "one experienced")
+    print(different_team, "different team")
+    print(num_reviews)
 
-# reviews = {}
-# for reviewer in reviewers:
-#     count = 0
-#     for assigned in reviewed.values():
-#         if reviewer in assigned:
-#             count += 1
-#     reviews[reviewer] = count
-
-# print(two_reviewers, "two reviewers")
-# print(one_experienced, "one experienced")
-# print(different_team, "different team")
-# print(reviews)
+test()
